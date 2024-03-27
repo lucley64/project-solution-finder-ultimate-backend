eef755b7115df0fe540a445fbd3a88a1eafba0bd
LilMDShin <lilian.michel.dansac@gmail.com>
2024-03-27 15:53:40 +0100

    Refactor

--- model/balance_sheet.py
+++ model/balance_sheet.py
@@ -317,41 +317,21 @@ def main():
 if __name__ == "__main__":
     main()
 
-# 
-# gains_dataset = "./model/tblgainrex.csv"
-# costs_dataset = "./model/tblcoutrex.csv"
-# currencies_dataset = "./model/tblmonnaie.csv"
-# 
-# df_gain_case_studies = read_csv(gains_dataset, sep = ',', engine = 'python', quotechar = '"')
-# df_cost_case_studies = read_csv(costs_dataset, sep = ',', engine = 'python', quotechar = '"')
-# df_currencies = read_csv(currencies_dataset, sep = ',', engine = 'python', quotechar = '"')
-# 
-# ex_query = "Comment faire pour réduire la consommation de mon compresseur d'air comprimé ?"
-# # ex_query = "J'aimerais avoir une régulation optimisée de mon groupe froid"
-# # ex_query = "C'est quoi la haute pression flottante"
-# # ex_query = "Je voudrais dimensionner un panneau solaire."
-# # ex_query = "Quel gain pour un variateur de vitesse ?"
-# # ex_query = "Quelles sont les meilleures solutions pour l'agro-alimentaire ?"
-# 
-# is_corpus_embeddings_to_update = False
-# 
-# if (is_corpus_embeddings_to_update):
-#     dataset_path = "./model/textSolModel.csv"
-#     all_df = read_csv(dataset_path, sep = ',', engine = 'python', quotechar = '"')
-# 
-#     # Takes only text corresponding to the name of the solution and its definition (as written in map_params.json)
-#     # all_df = all_df.loc[(all_df.indexdictionnaire == "nomsolution") | (all_df.indexdictionnaire == "principesolution")]
-# 
-#     # The french text is the most complete as of now
-#     df = semantic_search.get_df_one_lang_one_sol_per_row(semantic_search.Language.FRENCH.value, all_df)
-# 
-#     df.traductiondictionnaire = df.traductiondictionnaire.apply(semantic_search.preprocess)
-# 
-#     # Randomize the rows associated at each index
-#     df = df.sample(frac = 1).reset_index(drop = True)
-# 
-#     semantic_search.encode_text_sols(df)
-# 
-# res = semantic_search.semantic_search(ex_query)
-# 
-# balance_sheet(res, df_gain_case_studies, df_cost_case_studies, "EUR", df_currencies)
+is_corpus_embeddings_to_update = False
+
+if (is_corpus_embeddings_to_update):
+    dataset_path = "./model/textSolModel.csv"
+    all_df = read_csv(dataset_path, sep = ',', engine = 'python', quotechar = '"')
+
+    # Takes only text corresponding to the name of the solution and its definition (as written in map_params.json)
+    # all_df = all_df.loc[(all_df.indexdictionnaire == "nomsolution") | (all_df.indexdictionnaire == "principesolution")]
+
+    # The french text is the most complete as of now
+    df = semantic_search.get_df_one_lang_one_sol_per_row(semantic_search.Language.FRENCH.value, all_df)
+
+    df.traductiondictionnaire = df.traductiondictionnaire.apply(semantic_search.preprocess)
+
+    # Randomize the rows associated at each index
+    df = df.sample(frac = 1).reset_index(drop = True)
+
+    semantic_search.encode_text_sols(df)
